def0937528d93e4e32ca4b956d26f26381720bc3
Legolas <hau.pham-xuan@banvien.com.vn>
2023-02-16 16:08:25 +0700

    fix

--- src/Footer.jsx
+++
@@ -1,6 +0,0 @@
-import React, { useState } from 'react'
-
-export default function Footer() {
-  const [counter, setCounter] = useState('')
-  return <div>Footer</div>
-}
--- src/Header.jsx
+++
@@ -1,6 +0,0 @@
-import React, { useState } from 'react'
-
-export default function Header() {
-    const [counter, setCounter] = useState('')
-    return <div>Header</div>
-}
